e748d64a43a51cc53c9fb42c1402b71132e11827
Draxar <kamelotor@gmail.com>
2019-12-18 10:31:39 +0000

    Second star

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 # BRUTE FORCE blah...
+# 976 too high
 minPass = 245182
 maxPass = 790572
 passNum = 0
-passwords = []
+
+
 def checkNumber(num):
-  n = [int(x) for x in str(num)] 
-  if n[0]>n[1] or n[1]>n[2] or n[2]>n[3] or n[3]>n[4] or n[4]>n[5]:
-    return False
-  if n[0]!=n[1] and n[1]!=n[2] and n[2]!=n[3] and n[3]!=n[4] and n[4]!=n[5]:
-    return False
-  return True
+	n = [int(x) for x in str(num)]
+	if n[0] > n[1] or n[1] > n[2] or n[2] > n[3] or n[3] > n[4] or n[4] > n[5]:
+		return False
+	if ( 
+  not (n[0] == n[1] and n[0] != n[2]) and 
+  not (n[1] == n[2] and n[1] != n[0] and n[1] != n[3]) and 
+  not (n[2] == n[3] and n[2] != n[1] and n[2] != n[4]) and 
+  not (n[3] == n[4] and n[3] != n[2] and n[3] != n[5]) and 
+  not (n[4] == n[5] and n[4] != n[3])) :
+		return False
+	return True
+
 
 number = minPass
 while number <= maxPass:
-  if checkNumber(number):
-    passwords.append(number)
-    passNum += 1
-  number += 1
+	if checkNumber(number):
+		passNum += 1
+	number += 1
 
 print(passNum)
-
